02d0b50bc27eb0b6023a48f13a8811b42f55c230
km4 <m1leskm4@gmail.com>
2022-02-17 19:33:48 +0100

    random dog breeds

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>Lest go :)</div>
+  <div>
+    <img :src="url" alt="" />
+  </div>
 </template>
 
 <script>
+const API = 'https://dog.ceo/api/breeds/'
+const getTheList = API + 'list/all'
 export default {
-  //...
+  async asyncData({ params, $axios }) {
+    const { message, status } = await $axios.$get(
+      'https://dog.ceo/api/breeds/image/random'
+    )
+
+    return { url: message }
+  },
 }
 </script>
